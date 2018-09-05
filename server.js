bd9d40eeb5d79158e5caabaa7c4466f11adab9ee
danielwalton <walton.dan@gmail.com>
2018-09-05 13:55:36 -0400

    add open orders, improve results

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var tokens = require("./tokens.json");
 var config = require("./config.json");
 var ethexABI = require("./ethexABI");
 var abiDecode = require("abi-decoder");
+var BigNumber = require('bignumber.js');
 const fs = require('fs');
 var docs = fs.readFileSync("./docs.html");
 var tokenMap = {};
@@ -11,6 +12,7 @@ for (var token of tokens) {
     tokenMap[token.address.toLowerCase()] = token;
 }
 abiDecode.addABI(ethexABI);
+var minWei = new BigNumber(0.001).times(Math.pow(10, 18));
 
 var rpcCall = (method, params, result_callback) => {
     var optionspost = {
@@ -43,17 +45,29 @@ var getMarketData = (marketDataCallback) => {
     var blocksPer24Hours = (60 * 60 * 24) / 14.5;  //14.5 seconds per blocks on average. might need to be adjusted depending on blocktime.
     rpcCall("eth_blockNumber", [], (results) => {
         var lastBlock = parseInt(results.result);
-        var startBlock = lastBlock - blocksPer24Hours;
+        var startBlock = 4078264;
+        var rangeBlock = lastBlock - blocksPer24Hours;
         var startBlock = "0x" + startBlock.toString(16);
         var lastBlock = "0x" + lastBlock.toString(16);
         rpcCall("eth_getLogs", [{ "fromBlock": startBlock, "toBlock": lastBlock, "address": "0xb746aed479f18287dc8fc202fe06f25f1a0a60ae" }], (results) => {
             var logs = abiDecode.decodeLogs(results['result']);
-            for (var log of logs) {
+            var openOrders = {};
+            for (var log_idx in logs) {
                 //initialize
-                var tokenData = { last: null, lowestAsk: null, highestBid: null, volume: 0, high24hr: null, low24hr: null };
+                var log = logs[log_idx];
+
+                var tx = results['result'][log_idx]
+
+                var tokenData = { last: null, lowestAsk: null, highestBid: null, volume: new BigNumber(0), high24hr: null, low24hr: null };
                 var tokenAddress = null;
                 var token = null;
-                if (log.name === "MakeSellOrder" || log.name === "MakeBuyOrder" || log.name === "TakeSellOrder" || log.name === "TakeBuyOrder") {
+                if (log.name === "MakeSellOrder" ||
+                    log.name === "MakeBuyOrder" ||
+                    log.name === "TakeSellOrder" ||
+                    log.name === "TakeBuyOrder" ||
+                    log.name === "CancelBuyOrder" ||
+                    log.name === "CancelSellOrder") {
+                    tokenAddress = null;
                     for (var event of log.events) {
                         if (event.name === "token") {
                             tokenAddress = event.value;
@@ -65,107 +79,183 @@ var getMarketData = (marketDataCallback) => {
                         tokenData = marketData[tokenAddress];
                     }
                     token = tokenMap[tokenAddress];
-                    if (token === undefined) {
-                        console.log("undefined!!!", tokenAddress);
-                    } else {
-                    }
-                    var tokenAmount;
+                    var tokAmount;
                     var weiAmount;
+                    var orderHash;
                     for (var event of log.events) {
                         if (event.name === "tokenAmount")
-                            tokenAmount = event.value;
+                            tokAmount = new BigNumber(event.value);
                         if (event.name === "weiAmount")
-                            weiAmount = event.value;
+                            weiAmount = new BigNumber(event.value);
+                        if (event.name === "orderHash") {
+                            orderHash = event.value;
+                        }
                     }
-                    var weiPerToken = parseFloat(weiAmount) / parseFloat(tokenAmount);
-                    var adjust = 18 - parseInt(token.decimals);
-                    var price = weiPerToken * Math.pow(10, -adjust);
+                    var weiPerTok = weiAmount.dividedBy(tokAmount);
+
+                    var price = null;
+                    if (token)
+                        price = weiPerTok.dividedBy(Math.pow(10, 18 - token.decimals));//ETH per full token
 
                     if (log.name === "MakeSellOrder") { //ask
-                        if (tokenData.lowestAsk === null || price < tokenData.lowestAsk) {
-                            tokenData.lowestAsk = price;
-                        }
+                        var order = { type: log.name, tokenAddress: tokenAddress, price: price, weiPerTok: weiPerTok, weiAmount: weiAmount, tokAmount: tokAmount };
+                        openOrders[orderHash] = order;
                     }
                     if (log.name === "MakeBuyOrder") { //bid
-                        if (tokenData.highestBid === null || price > tokenData.highestBid) {
-                            tokenData.highestBid = price;
-                        }
+                        var order = { type: log.name, tokenAddress: tokenAddress, price: price, weiPerTok: weiPerTok, weiAmount: weiAmount, tokAmount: tokAmount };
+                        openOrders[orderHash] = order;
                     }
-                    if (log.name.startsWith("Take")) {
+                    if (log.name.startsWith("Take") && tx.blockNumber >= rangeBlock) {
                         tokenData.last = price;
-                        if (tokenData.high24hr === null || price > tokenData.high24hr)
+                        if (tokenData.high24hr === null || price.isGreaterThan(tokenData.high24hr))
                             tokenData.high24hr = price;
-                        if (tokenData.low24hr === null || price < tokenData.low24hr)
-                            tokenData.high24hr = price;
+                        if (tokenData.low24hr === null || price.isLessThan(tokenData.low24hr))
+                            tokenData.low24hr = price;
                     }
                     if (log.name === "TakeSellOrder") { //last price,volume,high24hr,low24hr
+
                         for (var event of log.events) {
                             if (event.name === "totalTransactionWei") {
-                                var vol = parseInt(event.value) / Math.pow(10, 18);
-                                tokenData.volume += vol;
+                                var totalTransactionWei = new BigNumber(event.value);
+                                if (openOrders[orderHash]) {
+                                    order = openOrders[orderHash];
+                                    order.weiAmount = order.weiAmount.minus(totalTransactionWei);
+                                    order.tokAmount = order.tokAmount.minus(totalTransactionWei.dividedBy(weiPerTok));
+                                    if (tx.blockNumber >= rangeBlock)
+                                        tokenData.volume = tokenData.volume.plus(totalTransactionWei.dividedBy(Math.pow(10, 18)));
+                                }
                             }
                         }
                     }
                     if (log.name === "TakeBuyOrder") { //last price,volume,high24hr,low24hr
                         for (var event of log.events) {
                             if (event.name === "totalTransactionTokens") {
-                                var dec = parseInt(token.decimals);
-                                var tok = parseInt(event.value) / Math.pow(10, dec);
-                                var vol = tok * price;
-                                tokenData.volume += vol;
+                                var totalTransactionTokens = new BigNumber(event.value);
+                                if (openOrders[orderHash]) {
+                                    order = openOrders[orderHash];
+                                    var totalTransactionWei = totalTransactionTokens.times(weiPerTok);
+                                    order.tokAmount = order.tokAmount.minus(totalTransactionTokens);
+                                    order.weiAmount = order.weiAmount.minus(totalTransactionTokens.times(weiPerTok));
+                                    if (tx.blockNumber >= rangeBlock)
+                                        tokenData.volume = tokenData.volume.plus(totalTransactionWei.dividedBy(Math.pow(10, 18)));
+                                }
                             }
                         }
+                    }
+                    if (log.name === "CancelBuyOrder") {
+                        delete openOrders[orderHash];
+                    }
+                    if (log.name === "CancelSellOrder") {
+                        delete openOrders[orderHash];
                     }
                     //tokenData and tokenAddress are now initialized.
                 }
 
             }
-            marketDataCallback(marketData);
+            //openOrders is the entire order book.
+            for (hash in openOrders) {
+                var openOrder = openOrders[hash];
+                if (openOrder.weiAmount.isLessThan(minWei)) {
+                    delete openOrders[hash];
+                    continue;
+                }
+                token = tokenMap[openOrder.tokenAddress];
+                tokenData = marketData[openOrder.tokenAddress];
+                if (openOrder.type === "MakeSellOrder" && (tokenData.lowestAsk === null || tokenData.lowestAsk.isGreaterThan(openOrder.price))) {
+                    tokenData.lowestAsk = openOrder.price;
+                }
+                if (openOrder.type === "MakeBuyOrder" && (tokenData.highestBid === null || tokenData.highestBid.isLessThan(openOrder.price))) {
+                    tokenData.highestBid = openOrder.price;
+                }
+            }
+            //format for eth
+            var ethOpenOrders = {};
+            for (hash in openOrders) {
+                var openOrder = openOrders[hash];
+                var type = "Buy";
+                if (openOrder.type === "MakeBuyOrder") {
+                    type = "Sell";
+                }
+                var token = tokenMap[openOrder.tokenAddress];
+                if (token) {
+                    ethOpenOrders[hash] = {
+                        type: type,
+                        priceEth: openOrder.price,
+                        tokenAmount: openOrder.tokAmount.dividedBy(Math.pow(10, token.decimals)),
+                        ethAmount: openOrder.weiAmount.dividedBy(Math.pow(10, 18)),
+                        tokenAddress: openOrder.tokenAddress,
+                        symbol: token.symbol
+                    };
+                } else {
+                }
+            }
+            marketDataCallback(marketData, ethOpenOrders);
         });
     });
-    
 }
 var MarketData = {};
+var OpenOrders = {};
 var refresh24Hour = () => {
-    getMarketData((marketData)=>{
+    getMarketData((marketData, openOrders) => {
         MarketData = marketData;
+        OpenOrders = openOrders;
     });
 }
 
 var server = http.createServer(function (req, res) {
-    // You can define here your custom logic to handle the request
-    // and then proxy the request.
-    //application/json-rpc
     if (req.method == "GET" && req.url == "/") {
         res.write(docs);
         res.end();
         return;
     }
-    if (req.method == 'GET' && req.url == "/ticker") {
+    if (req.method == 'GET') {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
         res.setHeader('Content-Type', 'application/json');
-        var results = {};
-        for (var address in MarketData) {
-            var token = tokenMap[address];
-            if (token) {
-                var pair = "ETH_"+token.symbol;
-                results[pair] = MarketData[address];
+        if (req.url === "/ticker24") {
+            var results = {};
+            for (var address in MarketData) {
+                var token = tokenMap[address];
+                if (token) {
+                    var pair = "ETH_" + token.symbol;
+                    results[pair] = MarketData[address];
+                }
             }
+            res.write(JSON.stringify(results, undefined, 2));
+            res.end();
+            return;
         }
-        res.write(JSON.stringify(results));
-        res.end();
-        return;
-    }
-    else if (req.method == 'POST') {
+        else if (req.url === "/openOrders") {
+            var results = {};
+            for (var hash in OpenOrders) {
+                var openOrder = OpenOrders[hash];
+                var token = tokenMap[openOrder.tokenAddress];
+                if (token) {
+                    var pair = "ETH_" + token.symbol;
+                    openOrder.pair = pair;
+                    openOrder.hash = hash;
+                    results[hash] = OpenOrders[hash];
+                }
+            }
+            res.write(JSON.stringify(results, undefined, 2));
+            res.end();
+            return;
+        }
+        else {
+            res.write(docs);
+            res.end();
+            return;
+        }
     }
 });
-
-getMarketData((marketData)=>{
+console.log("starting api server");
+getMarketData((marketData, openOrders) => {
     MarketData = marketData;
+    OpenOrders = openOrders;
     server.listen(config.server_port);
+    console.log("listening on", config.server_port);
 });
-setInterval(refresh24Hour,30*1000);
+setInterval(refresh24Hour, 30 * 1000);
 
 
